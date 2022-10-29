a073b4c3f5876835d4fc2ef78f839757c50f9ace
Lachlan Smith <30204915+lachlan-smith@users.noreply.github.com>
2022-10-29 11:10:15 +0100

    Ch2 - Colours - Basic Operations

--- src/colour.rs
+++ src/colour.rs
@@ -19,6 +19,54 @@ impl PartialEq for Colour {
     }
 }
 
+impl std::ops::Add<Colour> for Colour {
+    type Output = Colour;
+
+    fn add(self, rhs: Colour) -> Self::Output {
+        Self {
+            r: self.r + rhs.r,
+            g: self.g + rhs.g,
+            b: self.b + rhs.b,
+        }
+    }
+}
+
+impl std::ops::Sub<Colour> for Colour {
+    type Output = Colour;
+
+    fn sub(self, rhs: Colour) -> Self::Output {
+        Self {
+            r: self.r - rhs.r,
+            g: self.g - rhs.g,
+            b: self.b - rhs.b,
+        }
+    }
+}
+
+impl std::ops::Mul<Colour> for Colour {
+    type Output = Colour;
+
+    fn mul(self, rhs: Colour) -> Self::Output {
+        Self {
+            r: self.r * rhs.r,
+            g: self.g * rhs.g,
+            b: self.b * rhs.b,
+        }
+    }
+}
+
+impl std::ops::Mul<f64> for Colour {
+    type Output = Colour;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            r: self.r * rhs,
+            g: self.g * rhs,
+            b: self.b * rhs,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,5 +86,48 @@ mod tests {
         let colour2 = Colour::new(-0.5, 0.4, 1.7);
 
         assert_eq!(colour1, colour2)
+    }
+
+    #[test]
+    fn add_colours() {
+        let colour1 = Colour::new(0.9, 0.6, 0.75);
+        let colour2 = Colour::new(0.7, 0.1, 0.25);
+
+        let result = colour1 + colour2;
+        let expected = Colour::new(1.6, 0.7, 1.0);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn sub_colours() {
+        let colour1 = Colour::new(0.9, 0.6, 0.75);
+        let colour2 = Colour::new(0.7, 0.1, 0.25);
+
+        let result = colour1 - colour2;
+        let expected = Colour::new(0.2, 0.5, 0.5);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn multiply_colours() {
+        let colour1 = Colour::new(1.0, 0.2, 0.4);
+        let colour2 = Colour::new(0.9, 1.0, 0.1);
+
+        let result = colour1 * colour2;
+        let expected = Colour::new(0.9, 0.2, 0.04);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn multiply_colour_by_scalar() {
+        let colour = Colour::new(0.2, 0.3, 0.4);
+
+        let result = colour * 2.0;
+        let expected = Colour::new(0.4, 0.6, 0.8);
+
+        assert_eq!(result, expected);
     }
 }
